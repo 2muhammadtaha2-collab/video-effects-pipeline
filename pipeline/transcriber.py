f134fe2e36696871c4118e0ae55df37a0138b3f1
Hamza Azam <hamzaazam2375@gmail.com>
2026-06-05 12:21:03 +0500

    libs

--- pipeline/transcriber.py
+++ pipeline/transcriber.py
@@ -1,1 +1,9 @@
 # transcription logic here
+# first install faster whisper using: pip install faster-whisper   
+
+import json
+import os
+import subprocess
+
+from faster_whisper import WhisperModel
+
